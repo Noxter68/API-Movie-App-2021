1ce3f156128e3815618eb5aa6c697befa726e924
David <davidplanchon.68@gmail.com>
2021-02-16 13:27:14 +0100

    updated index

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Welcome on my personal Movie api');
+    res.send("Welcome on my personal Movie API for David's AppMovie.");
  })
 
  module.exports = router;
